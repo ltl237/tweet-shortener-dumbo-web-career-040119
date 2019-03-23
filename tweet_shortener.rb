4468e677f4787792425bd591ff2e96a4bfff5bdb
Lorenzo Llamas <ltl924@gmail.com>
2019-03-23 21:17:30 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+```
+"hello" becomes 'hi'
+"to, two, too" become '2' 
+"for, four" become '4'
+'be' becomes 'b'
+'you' becomes 'u'
+"at" becomes "@" 
+"and" becomes "&"
+```
+
+def dictionary(your_str)
+  
+end
